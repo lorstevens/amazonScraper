ebfed30d1e8949bf776592f0af54184f8befb4a7
lorstevens <lornacstevens@gmail.com>
2018-07-12 21:28:17 -0500

    updated scraper

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # amazonScraper
+
+I used Puppeteer and Cheerio to scrape books written by Ernest Hemnigway from Amazon. 
+
+To get started, run the following command:
+
+``` 
+node scrape.js
+```
+
+Results will be printed in JSON to a text.txt file
--- scrape.js
+++ scrape.js
@@ -3,8 +3,7 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const path = 'text.txt'
 
-
-async function run(error, data) {
+let run = async (error, data) => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
 
@@ -19,7 +18,7 @@ async function run(error, data) {
     const $ = cheerio.load(result);
     let contentData = [];
 
-    // s-access-detail-page 
+
     $('.s-result-item').each(function (i, element) {
         if (error) {
             console.error(err)
@@ -28,65 +27,94 @@ async function run(error, data) {
 
             let el = $(this);
 
+            let id = $(this).attr('id')
+          
             let rootRight = el.children()
                 .children()
                 .children()
                 .children('.a-fixed-left-grid-col.a-col-right')
                 .children('.a-row.a-spacing-small')
 
+            //Find the titles
             let title = rootRight.find('div.a-row.a-spacing-none > a[title]')
                 .text();
 
+            // Find the authors
             let author = rootRight.find('div.a-row.a-spacing-none')
                 .next()
                 .children()
+                .children('a')
                 .text();
 
-            let price = rootRight.next()
+            //Find the price
+            //Find the Whole Number
+            let priceWhole = rootRight.next()
                 .children('div > .a-column.a-span7')
                 .children('.a-row.a-spacing-none')
                 .children('a')
                 .children('.a-color-base.sx-zero-spacing')
                 .children('.sx-price')
-                .first('span.sx-price-whole')
+                .children('span.sx-price-whole')
                 .text()
                 .trim();
 
+            //Find the Decimal Number
+            let priceDecimal = rootRight.next()
+                .children('div > .a-column.a-span7')
+                .children('.a-row.a-spacing-none')
+                .children('a')
+                .children('.a-color-base.sx-zero-spacing')
+                .children('.sx-price')
+                .children('.sx-price-fractional')
+                .text()
+                .trim();
+
+            //Combine whole and decimal numbers
+            let price = priceWhole + '.' + priceDecimal
+
+            //Find the description
             let description = rootRight.next()
                 .children('.a-column.a-span7')
                 .children('.a-row.a-spacing-small')
                 .children('span')
                 .text();
 
+            let date = rootRight.children()
+                .first()
+                .children('span.a-size-small.a-color-secondary')
+                .text()
+                
+
             let rootLeft = el.children()
                 .children()
                 .children()
                 .children('.a-fixed-left-grid-col.a-col-left')
 
+            //Find the Source URL
             let link = rootLeft.children()
                 .children()
                 .children('.a-link-normal.a-text-normal')
                 .attr('href');
 
+            //Find the image URL
             let imageUrl = rootLeft.children()
                 .children()
                 .children('.a-link-normal.a-text-normal')
                 .children()
                 .attr('src');
 
-
-            let productDimension;
-            let weight;
-
+            //Push into the contentData Array
             if (title) {
                 contentData.push({
                     product: {
+                        id: id,
                         title: title,
                         description: description,
                         sourceUrl: link,
                         imageUrl: imageUrl,
-                        price: price.replace(/\s+/g, ''),
-                        author: author
+                        price: price,
+                        author: author,
+                        date: date
                     }
                 });
             }
@@ -101,8 +129,6 @@ async function run(error, data) {
     });
 
     await browser.close();
-
 }
-
 run();
 
